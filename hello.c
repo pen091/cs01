fbf5e256aed705edaf8718f8f9491abf4a98c1dd
pen091 <princeekenendioziri@gmail.com>
2025-12-27 22:41:02 +0000

    cs01 first stage code

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	/* putchar 
+	 * printf
+	 * putc
+	 * puts */
+
+	int info = printf("hello world!\n");
+	printf("return of printf : %d\n", info);
+	return 0;
+}
